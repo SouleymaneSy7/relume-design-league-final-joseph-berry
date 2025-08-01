a4567fe6a749487e21d28f127ad73d1cbc359ab0
souleymanesy7 <souleymanesyservices@gmail.com>
2025-08-01 16:25:47 +0000

    feat: add the button components in style guide

--- src/app/style-guide/page.tsx
+++ src/app/style-guide/page.tsx
@@ -1,5 +1,6 @@
-import Button from '@/components/common/Button'
+import { Button } from '@/components/common/Button'
 import Title from '@/components/common/Title'
+import { ArrowLeftIcon, ArrowRightIcon } from 'lucide-react'
 
 const page = () => {
   return (
@@ -9,12 +10,35 @@ const page = () => {
       </Title>
 
       <div>
-        <Title level="h2" className="text-heading-2 mb-4">
+        <Title level="h2" className="text-heading-2 mb-4 font-medium">
           Button:
         </Title>
 
-        <div>
-          <Button variant="default">Default</Button>
+        <div className="space-y-3">
+          <Button variant="default" className="border-dark-clr border-2">
+            Order Now
+          </Button>
+          <Button variant="secondary">Order Now</Button>
+          <Button variant="outline">Subscribe</Button>
+          <div className="flex items-center gap-4">
+            <Button variant={'arrow_ghost'}>
+              <ArrowLeftIcon />
+            </Button>
+
+            <Button variant={'arrow_ghost'}>
+              <ArrowRightIcon />
+            </Button>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Button variant={'arrow'}>
+              <ArrowLeftIcon />
+            </Button>
+
+            <Button variant={'arrow'}>
+              <ArrowRightIcon />
+            </Button>
+          </div>
         </div>
       </div>
     </main>
