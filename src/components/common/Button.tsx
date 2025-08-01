315753a89f9cb1204d70002f844bb45ab4749a71
souleymanesy7 <souleymanesyservices@gmail.com>
2025-08-01 16:25:10 +0000

    feat: update the button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,57 +1,49 @@
 import * as React from 'react'
-
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
-const buttonVariants = cva({
-  base: 'whitespace-nowrap flex items-center justify-center capitalize transition-colors cursor-pointer',
-  variants: {
-    variant: {
-      default:
-        'bg-light-clr text-default-clr shadow-xs hover:bg-transparent border-2 border-light-clr',
-      outline:
-        'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-      secondary:
-        'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-      ghost:
-        'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-      link: 'text-primary underline-offset-4 hover:underline',
+const buttonVariants = cva(
+  'whitespace-nowrap flex items-center justify-center capitalize transition-colors cursor-pointer rounded-full',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-light-clr text-default-clr border-2 border-dark-clr hover:bg-transparent transition-colors h-14 w-[172px] py-2 lg:w-[112px] lg:h-10',
+        secondary:
+          'bg-dark-clr text-text-light-clr hover:bg-dark-clr/80 h-14 w-[172px] py-2',
+        outline:
+          'border border-light-clr bg-dark-clr text-light-clr w-[120px] h-12',
+        arrow:
+          'rounded-full bg-primary-clr grid place-items-center h-[96px] w-[96px]',
+        arrow_ghost: 'grid place-items-center',
+      },
     },
-    size: {
-      default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-      sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-      lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-      icon: 'size-9',
+    defaultVariants: {
+      variant: 'default',
     },
-  },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
-  },
-})
+  }
+)
 
 function Button({
   className,
   variant,
-  size,
   asChild = false,
-  ...delegatedProps
+  ...props
 }: React.ComponentProps<'button'> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
-    variant?: string
-    size?: string
   }) {
-  const Component = asChild ? Slot : 'button'
+  const Comp = asChild ? Slot : 'button'
 
   return (
-    <Component
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...delegatedProps}
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, className }))}
+      {...props}
     />
   )
 }
 
-export default Button
+export { Button, buttonVariants }
