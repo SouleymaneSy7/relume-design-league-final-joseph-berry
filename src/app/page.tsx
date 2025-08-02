c55eb95a6394ba0c30e293054d1ddfe21d801587
souleymanesy7 <souleymanesyservices@gmail.com>
2025-08-02 18:04:34 +0000

    feat: add the join section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Container from '@/components/common/Container'
+import JoinSection from '@/components/layouts/JoinSection'
+
 export default function Home() {
-  return <div className="h-screen">Next Starting Project!!!</div>
+  return (
+    <Container as={'section'}>
+      <div className="h-screen">Next Starting Project!!!</div>
+      <JoinSection />
+    </Container>
+  )
 }
