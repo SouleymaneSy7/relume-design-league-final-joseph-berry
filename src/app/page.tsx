c907fa395a3f16dbb7340a49c043ea30b4d5d4aa
souleymanesy7 <souleymanesyservices@gmail.com>
2025-08-04 05:22:53 +0000

    feat: add the reviews section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Container from '@/components/common/Container'
 import CommuteSection from '@/components/layouts/CommuteSection'
 import JoinSection from '@/components/layouts/JoinSection'
+import ReviewSection from '@/components/layouts/ReviewSection'
 
 export default function Home() {
   return (
     <Container as={'section'}>
       <CommuteSection />
+      <ReviewSection />
       <JoinSection />
     </Container>
   )
