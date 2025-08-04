7a6a15934fe38a344b26c11d34cf718ee63c05d6
souleymanesy7 <souleymanesyservices@gmail.com>
2025-08-04 16:54:21 +0000

    feat: add the card section constants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Container from '@/components/common/Container'
+import CardSection from '@/components/layouts/CardSection'
 import CommuteSection from '@/components/layouts/CommuteSection'
 import JoinSection from '@/components/layouts/JoinSection'
 import ReviewSection from '@/components/layouts/ReviewSection'
@@ -6,6 +7,7 @@ import ReviewSection from '@/components/layouts/ReviewSection'
 export default function Home() {
   return (
     <Container as={'section'}>
+      <CardSection />
       <CommuteSection />
       <ReviewSection />
       <JoinSection />
